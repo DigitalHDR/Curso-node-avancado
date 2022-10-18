bc50e803699d71dcb3de68a8c3704de7eca8e902
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-10-18 12:22:35 -0300

    MODULO HTTP com url

---
+++ 3_CORE_MODULES/4_http_com_url/index.js
@@ -0,0 +1,24 @@
+const http = require('http')
+// const url = require('url')
+
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  const urlInfo = require('url').parse(req.url, true)
+  const name = urlInfo.query.name
+
+  res.statusCode = 200
+  res.setHeader('Constenty_Type', 'text/html')
+
+  if (!name) {
+    res.end(
+      '<h1>prencha o seu nome:</h1><form method="GET"><input type="text" name="name"/> <input type="submit" value="Enviar"> </form>'
+    )
+  } else {
+    res.end(`<h1>seja bem-vindo ${name}`)
+  }
+})
+
+server.listen(port, () => {
+  console.log(`servidor rodando na porta: ${port}`)
+})
