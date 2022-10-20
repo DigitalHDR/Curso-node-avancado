33eae9af12b27371cbd7ca280078cb995ad75d28
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-10-20 12:46:15 -0300

    inicialização do sistema

---
+++ 5_ACCOUNTS/index.js
@@ -0,0 +1,31 @@
+// modulos externos
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+// modulos internos
+const fs = require('fs')
+
+operation()
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'action',
+        message: 'O que você desaja fazer?',
+        choices: [
+          'Criar Conta',
+          'Consultar Saldo',
+          'Depositar',
+          'Sacar',
+          'Sair',
+        ],
+      },
+    ])
+    .then(answer => {
+      const action = answer['action']
+      console.log(action)
+    })
+    .catch(err => console.log(err))
+}
