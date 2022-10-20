69bee8c94e1ee0a762dd6d7a80d4ba02ad00c8e8
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-10-20 13:28:41 -0300

    finalizando criação de conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -25,7 +25,59 @@ function operation() {
     ])
     .then(answer => {
       const action = answer['action']
-      console.log(action)
+
+      if (action === 'Criar Conta') {
+        createAccount()
+      }
     })
     .catch(err => console.log(err))
+}
+
+// create an account
+function createAccount() {
+  console.log(chalk.bgGreen.black('parabéns por escolher o nosso produto!'))
+  console.log(chalk.green('defina as opções da sua conta a seguir'))
+
+  buildAccount()
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'digite um nome para sua conta',
+      },
+    ])
+    .then(answer => {
+      const accountName = answer['accountName']
+
+      console.info(accountName)
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('esta conta já existe, escolha outra nome!')
+        )
+        buildAccount()
+        return
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        function (err) {
+          console.log(err)
+        }
+      )
+
+      console.log(chalk.green('parabéns, sua conta foi criadaa!'))
+      operation()
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
