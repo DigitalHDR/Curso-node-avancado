06d26f93c67772fc17d8ee2999629286ec028d74
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2022-10-21 13:03:25 -0300

    criação de conta

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -81,3 +81,48 @@ function buildAccount() {
       console.log(err)
     })
 }
+
+// create an account
+function createAccount() {
+  console.log(chalk.bgGreen.black('parabens por escolher o nosso banco'))
+  console.log(chalk.green('defina as opções da sua conta a seguir'))
+
+  buildAccount()
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'digite um nome para a sua conta',
+      },
+    ])
+    .then(answer => {
+      const accountName = answer['accountName']
+      console.info(accountName)
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black('esta conta já existe, escolha outra nome')
+        )
+        buildAccount()
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        function (err) {
+          console.log(err)
+        }
+      )
+
+      console.log(chalk.green('parabéns, a sua conta foi criada'))
+      operation()
+    })
+    .catch(err => console.log(err))
+}
